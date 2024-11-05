f8a01b95ae5d1813c9065f7f722667ea0ce832e0
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-06 00:32:22 +0530

    Q4 solution is added

---
+++ MinorAssignment3/Q4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void mergeArrays(int a[], int b[], int m, int n, int merged[]) {
+    int i = 0, j = 0, k = 0;
+
+    while (i < m && j < n) {
+        if (a[i] < b[j]) {
+            merged[k++] = a[i++];
+        } else {
+            merged[k++] = b[j++];
+        }
+    }
+
+    while (i < m) {
+        merged[k++] = a[i++];
+    }
+
+    while (j < n) {
+        merged[k++] = b[j++];
+    }
+}
+
+int main() {
+    int a[] = {1, 3, 5, 7};
+    int b[] = {2, 4, 6, 8};
+    int m = sizeof(a) / sizeof(a[0]);
+    int n = sizeof(b) / sizeof(b[0]);
+    int merged[m + n];
+
+    mergeArrays(a, b, m, n, merged);
+
+    printf("Merged array: ");
+    for (int i = 0; i < m + n; i++) {
+        printf("%d ", merged[i]);
+    }
+
+    return 0;
+}
