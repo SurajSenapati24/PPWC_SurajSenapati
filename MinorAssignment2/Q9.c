8ed2a5fc5bb41856418e9db2390a89a07bdf4c6f
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-05 23:47:54 +0530

    updated

--- MinorAssignment2/Q9.c
+++ MinorAssignment2/Q9.c
@@ -3,36 +3,23 @@
 int main() {
     char ch, i, j, k;
     int space;
-
-    // Get the input character from the user
     printf("Enter the choice of the character: ");
     scanf("%c", &ch);
-
-    // Determine the number of rows based on the input character
     int rows = ch - 'A' + 1;
-
-    // Loop to print each row
     for (i = 0; i < rows; i++) {
-        // Print the ascending part of the row
         for (j = 'A'; j <= ch - i; j++) {
             printf("%c ", j);
         }
-
-        // Print the spaces in the middle
         space = 2 * i - 1;
         for (k = 0; k < space; k++) {
             printf("  ");
         }
-
-        // Print the descending part of the row
         for (j = ch - i; j >= 'A'; j--) {
-            if (j != ch || i != 0) { // Avoid printing the middle element twice in the first row
+            if (j != ch || i != 0) {
                 printf("%c ", j);
             }
         }
-
         printf("\n");
     }
-
     return 0;
 }
