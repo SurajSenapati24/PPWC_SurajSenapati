df6432230c7ef047e0a60f4da48edc7398a05b09
Suraj Senapati <surajsenapati58@gmail.com>
2024-11-22 01:09:24 +0530

    Q10 solution added

---
+++ MinorAssignment4/Q10.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a[]={120,502,118,188,106,447};
+    int *ptr1=&a[0];
+    int *ptr2=&a[1];
+    int *ptr3=&a[2];
+    int *ptr4=&a[3];
+    int *ptr5=&a[4];
+    int *ptr6=&a[5];
+    printf("a[0]: %d\n",*ptr1);
+    printf("a[1]: %d\n",*ptr2);
+    printf("a[2]: %d\n",*ptr3);
+    printf("a[3]: %d\n",*ptr4);
+    printf("a[4]: %d\n",*ptr5);
+    printf("a[5]: %d\n",*ptr6);
+    return 0;
+}
