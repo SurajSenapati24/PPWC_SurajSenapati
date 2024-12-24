751b1904cfe7774d598acc4b504b50e8cd208d1b
Suraj Senapati <surajsenapati58@gmail.com>
2024-12-24 22:12:45 +0530

    Q3 solution added

--- MinorAssignment5/Q3.c
+++ MinorAssignment5/Q3.c
@@ -14,5 +14,8 @@ int main(){
 }
 //Output: 19
 /*
-
+**a=**(start address of row 0 in a)=> *(address of 1st element in row 0 in a)=>1
+**a+2=1+2=3
+*(a+**a+2)=*(start address of row 0 in a + 1 + 2)=>*(address of 3rd row in a)=> address of first element in row 3
+*(*(a+**a+2)+3)=>*(address of first element in 3rd row + 3)=*(address of 4th element in 3rd row)=19
 */
