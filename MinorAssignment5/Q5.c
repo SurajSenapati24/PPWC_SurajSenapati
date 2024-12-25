d1d5fbb46cb40b46e6b2a22cdc551f476b5f580a
Suraj Senapati <surajsenapati58@gmail.com>
2024-12-25 21:23:28 +0530

    update

--- MinorAssignment5/Q5.c
+++ MinorAssignment5/Q5.c
@@ -9,4 +9,25 @@
 /*
 Output:
 1,1,1
+
+Explanation:
+
+First printf:
+printf("%d,", *a[2]);
+a[2] points to the first element of the third row, i.e., &a[2][0].
+*a[2] dereferences a[2], which gives 1 (the value at a[2][0]).
+
+Second printf:
+printf("%d,", a[2][0]);
+a[2][0] directly accesses the first element of the third row, which is 1.
+
+
+Third printf:
+printf("%d ", **(a+1+('b'-'a')));
+('b' - 'a') evaluates to 1 (since the ASCII difference between 'b' and 'a' is 1).
+a + 1 points to the second row (a[1]).
+(a + 1 + 1) is a + 2, which points to the third row (a[2]).
+*(a + 2) dereferences to the third row, i.e., a[2].
+**(a + 2) dereferences a[2] to give 1 (value at a[2][0]).
+
 */
