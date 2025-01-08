06b0693fe221bd87901bebed2c1890eae380e53a
Suraj Senapati <surajsenapati58@gmail.com>
2025-01-08 18:18:40 +0530

    update

--- MinorAssignment4/q21.c
+++ MinorAssignment4/q21.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 void arrange(int *a, int *b);
-
 void arrange(int *a, int *b)
 {
     if (*a > *b)
@@ -11,33 +9,25 @@ void arrange(int *a, int *b)
         *b = temp;
     }
 }
-
 void main()
 {
     int n1, n2, n3, n4, n5, n6;
-
     printf("Enter SIX numbers separated by blanks> ");
     scanf("%d %d %d %d %d %d", &n1, &n2, &n3, &n4, &n5, &n6);
-
     arrange(&n1, &n2);
     arrange(&n1, &n3);
     arrange(&n1, &n4);
     arrange(&n1, &n5);
     arrange(&n1, &n6);
-
     arrange(&n2, &n3);
     arrange(&n2, &n4);
     arrange(&n2, &n5);
     arrange(&n2, &n6);
-
     arrange(&n3, &n4);
     arrange(&n3, &n5);
     arrange(&n3, &n6);
-
     arrange(&n4, &n5);
     arrange(&n4, &n6);
-
     arrange(&n5, &n6);
-
     printf("The numbers in ascending order are: %d %d %d %d %d %d\n", n1, n2, n3, n4, n5, n6);
 }
