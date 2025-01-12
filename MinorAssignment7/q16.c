d79c4ad650f6fadbd505a1d32f66df6263305239
Suraj Senapati <surajsenapati58@gmail.com>
2025-01-13 01:27:53 +0530

    q16 solution added

---
+++ MinorAssignment7/q16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+typedef struct {
+    char name[30];
+    double diameter;
+    int moons;
+    double or_time;  // Orbital time
+    double ro_time;  // Rotation time
+} planet_t;
+
+int main() {
+    // Declare two variables of type planet_t
+    planet_t earth, mars;
+}
