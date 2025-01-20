f35b38255d29ea5671630ace5fefdf57e49a6b98
Suraj Senapati <surajsenapati58@gmail.com>
2025-01-20 22:27:31 +0530

    update

--- MinorAssignment4/Q15.c
+++ MinorAssignment4/Q15.c
@@ -4,14 +4,11 @@ void main()
 {
     int a, b, c, d;
     int *ptr1 = &a, *ptr2 = &b, *ptr3 = &c, *ptr4 = &d;
-
     *ptr1 = 55;
     *ptr2 = 105;
     *ptr3 = 89;
     *ptr4 = 68;
-
     int *arr[4] = {ptr1, ptr2, ptr3, ptr4};
-
     int max = *arr[0]; 
     for (int i = 1; i < 4; i++){ 
         if (*arr[i] > max) 
