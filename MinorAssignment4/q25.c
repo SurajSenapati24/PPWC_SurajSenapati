3730ef5453c61a00259c75ded7cd40a35fddfec6
Suraj Senapati <surajsenapati58@gmail.com>
2025-01-22 20:17:58 +0530

    update

--- MinorAssignment4/q25.c
+++ MinorAssignment4/q25.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-
 void main()
 {
     int arr[5] = {332, 441, 515, 696, 77};
-
     int *ptr = arr;
     int max = ptr[0];
-
     for (int i = 0; i < 5; i++)
     {
         if (ptr[i] > max)
             max = ptr[i];
     }
-
     printf("Max: %d\n", max);
 }
